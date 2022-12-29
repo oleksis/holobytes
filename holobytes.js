36f1d14586ee5ddf4c5ebc4621ec5f7b4bd3b2fe
Oleksis Fraga Menéndez <oleksis.fraga@gmail.com>
2022-12-29 03:22:42 -0500

    Use fetch API

--- holobytes.js
+++ holobytes.js
@@ -1,28 +1,76 @@
-var blogURL = "https://blog.holopin.io"
-var holoBytesPath = "/holobyte/collect"
+var blogURL = "https://blog.holopin.io";
+var holoBytesPath = "/holobyte/collect";
 
-function getHoloBytesLinks(holoBytesPath = "/holobyte/collect"){
-    const hbLinks = new Object()
-    var holobytesLinks = document.getElementsByTagName("a");
-    for(var counter = 0; counter < holobytesLinks.length; counter++){
-        const _url = new URL(holobytesLinks[counter].href)
-        if (holobytesLinks[counter].href.includes(holoBytesPath)){
-            hbLinks[_url.href] = holobytesLinks[counter];
-            hbLinks[_url.href].style.background = "red";
-        }
+function getHoloBytesLinks(
+  doc = document,
+  holoBytesPath = "/holobyte/collect"
+) {
+  const hbLinks = new Object();
+  var holobytesLinks = doc.getElementsByTagName("a");
+  for (var counter = 0; counter < holobytesLinks.length; counter++) {
+    const _url = new URL(holobytesLinks[counter].href);
+    if (holobytesLinks[counter].href.includes(holoBytesPath)) {
+      hbLinks[_url.href] = holobytesLinks[counter];
+      hbLinks[_url.href].style.background = "red";
     }
-    return hbLinks;
+  }
+  return hbLinks;
 }
 
-// For each Post
-const posts = getHoloBytesLinks("/posts/")
+result = await fetch(blogURL)
+  .then(function (response) {
+    return response.text();
+  })
+  .then(function (html) {
+    const hbLinks = {};
+    let parser = new DOMParser();
+    let doc = parser.parseFromString(html, "text/html");
+    let links = getHoloBytesLinks(doc);
 
-console.log( "HoloBytes hunted: " + Object.keys(getHoloBytesLinks()).length);
+    for (const [key, value] of Object.entries(links)) {
+      hbLinks[key] = value;
+    }
+
+    var posts = getHoloBytesLinks(doc, "/posts/");
+
+    Object.keys(posts).map((url) => {
+      fetch(url)
+        .then(function (response) {
+          return response.text();
+        })
+        .then(function (html) {
+          let parser = new DOMParser();
+          let doc = parser.parseFromString(html, "text/html");
+          let links = getHoloBytesLinks(doc);
+
+          for (const [_url, element] of Object.entries(links)) {
+            hbLinks[_url] = element;
+          }
+        })
+        .catch(function (err) {
+          console.log("Failed to fetch post page: ", err);
+        });
+    });
+
+    return hbLinks;
+  })
+  .catch(function (err) {
+    console.log("Failed to fetch page: ", err);
+  });
+
+setTimeout(() => {
+  console.log("HoloBytes hunted: " + Object.keys(result).length);
+  Object.keys(result).map((url) => {
+    console.log(url);
+  });
+}, 5000);
 
 function huntHoloBytesByClass() {
-    var holobytes = document.getElementsByClassName("hover:no-underline hover:text-neutral-700 text-sm font-base");
-    for(var counter = 0; counter < holobytes.length; counter++){
-        holobytes[counter].style.background = "red";
-    }
-    return holobytes;
+  var holobytes = document.getElementsByClassName(
+    "hover:no-underline hover:text-neutral-700 text-sm font-base"
+  );
+  for (var counter = 0; counter < holobytes.length; counter++) {
+    holobytes[counter].style.background = "red";
+  }
+  return holobytes;
 }
