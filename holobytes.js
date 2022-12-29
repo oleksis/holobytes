95902e52e4dc0a53907b35cebf7ee3d322c73bb8
Oleksis Fraga Menéndez <oleksis.fraga@gmail.com>
2022-12-28 21:16:41 -0500

    Add getHoloBytes by Links A tag

--- holobytes.js
+++ holobytes.js
@@ -1,9 +1,28 @@
-function huntHoloBytes() {
+var blogURL = "https://blog.holopin.io"
+var holoBytesPath = "/holobyte/collect"
+
+function getHoloBytesLinks(holoBytesPath = "/holobyte/collect"){
+    const hbLinks = new Object()
+    var holobytesLinks = document.getElementsByTagName("a");
+    for(var counter = 0; counter < holobytesLinks.length; counter++){
+        const _url = new URL(holobytesLinks[counter].href)
+        if (holobytesLinks[counter].href.includes(holoBytesPath)){
+            hbLinks[_url.href] = holobytesLinks[counter];
+            hbLinks[_url.href].style.background = "red";
+        }
+    }
+    return hbLinks;
+}
+
+// For each Post
+const posts = getHoloBytesLinks("/posts/")
+
+console.log( "HoloBytes hunted: " + Object.keys(getHoloBytesLinks()).length);
+
+function huntHoloBytesByClass() {
     var holobytes = document.getElementsByClassName("hover:no-underline hover:text-neutral-700 text-sm font-base");
     for(var counter = 0; counter < holobytes.length; counter++){
         holobytes[counter].style.background = "red";
     }
     return holobytes;
 }
-
-console.log( "HoloBytes hunted: " + huntHoloBytes().length);
